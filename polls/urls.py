c05f5841867dbf08a4c21bf5ea1864a5f94c0484
jungdoyoon <jungdo8016@naver.com>
2020-05-20 14:57:48 +0900

    ..

---
+++ polls/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+
+app_name='polls'
+
+urlpatterns = [
+
+     path('', views.index, name='index'),
+     path('<int:question_id>/', views.detail, name='detail'),
+     path('<int:question_id>/vote/', views.vote, name='vote'),
+     path('<int:question_id>/results/', views.results, name='result'),
+
+
+
+
+
+    #path('polls/', views.index,name='index'),
+    #path('polls/<int:question_id>/', views.detail, name='detail'),
+    #path('polls/<int:question_id>/vote/', views.vote, name='vote'),
+    #path('polls/<int:question_id>/results/', views.results, name='result'),
+]
